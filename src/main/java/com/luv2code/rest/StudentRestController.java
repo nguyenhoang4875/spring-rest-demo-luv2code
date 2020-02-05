3d6f63ed9314ced9a38861255cdc863c8e1c468b
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-05 08:20:26 +0700

    exception handler generic

--- src/main/java/com/luv2code/rest/StudentRestController.java
+++ src/main/java/com/luv2code/rest/StudentRestController.java
@@ -66,7 +66,23 @@ public class StudentRestController {
         // return ResponseEntity
 
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
 
+    // add another exception handler ... to catch any exception (catch all)
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception ex){
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(ex.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
     }
 
 }
