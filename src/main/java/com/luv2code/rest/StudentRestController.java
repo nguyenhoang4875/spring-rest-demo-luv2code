5fd93bfec71e2d46572d89bcdecdf5abbf5e3a56
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-05 08:30:21 +0700

    global exception handler with @ControllerAdvice

--- src/main/java/com/luv2code/rest/StudentRestController.java
+++ src/main/java/com/luv2code/rest/StudentRestController.java
@@ -50,39 +50,4 @@ public class StudentRestController {
 
             return students.get(studentId);
     }
-
-    // Add an exception handler using @ExceptionHandler
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex){
-
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(ex.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-
-        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler ... to catch any exception (catch all)
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception ex){
-
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(ex.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-
-        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
-    }
-
 }
